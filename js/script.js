0b3d6d81eefc97fe8a3fe9ec5f68b8bc64425027
ulisse-fontanot <ulissef99@gmail.com>
2021-01-14 17:48:36 +0100

    controllo array non funziona

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var listaRandom = [];
 var listaUtente = [];
+var giusti = [];
 
 //LISTA RANDOM
 while(listaRandom.length < 5){
@@ -18,17 +19,21 @@ function myFunction() {
     var numeroUtente = parseInt(prompt("inserisci numero"));
     if (numeroUtente <= 20 && numeroUtente > 0) {
       if (!listaUtente.includes(numeroUtente)) {
-        numeroUtente = parseInt(prompt("inserisci numero"));
         listaUtente.push(numeroUtente);
       }
     }else {
       alert("inserisci un numero tra 1 e 20")
     }
-
   }
+  console.log(listaUtente);
 }
-console.log(listaUtente);
 
+//CONTROLLO VITTORIA
+
+if (listaUtente.includes(random)) {
+  console.log("hai indovinato");
+  alert("hai indovinato");
+}
 
 //FUNZIONI RIUTILIZZABILI
 function numeri(min,max) {
