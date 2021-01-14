4190c2b156927dc94cc68f76bb40daf5c041b414
ulisse-fontanot <ulissef99@gmail.com>
2021-01-14 16:51:14 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var listaRandom = [];
+var listaUtente = [];
+
+while (listaRandom.length < 5) {
+  var random = numeri(1,20);
+  (listaRandom).push(random);
+}
+
+console.log(listaRandom);
+
+function numeri(min,max) {
+return Math.floor(Math.random() * (max - min) ) + min;
+}
