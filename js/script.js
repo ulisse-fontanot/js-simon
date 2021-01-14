605a22d2051a9e19372bc23b5e7e9de5a0a6e072
ulisse-fontanot <ulissef99@gmail.com>
2021-01-14 17:20:43 +0100

    popolare lista utente

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var listaRandom = [];
 var listaUtente = [];
 
+setTimeout(myFunction, 3000);
+
 while(listaRandom.length < 5){
   var random = numeri(1,20);
   if (!listaRandom.includes(random)) {
@@ -9,6 +11,24 @@ while(listaRandom.length < 5){
 }
 
 console.log(listaRandom);
+alert(listaRandom);
+
+
+function myFunction() {
+  while(listaUtente.length < 5) {
+    var numeroUtente = parseInt(prompt("inserisci numero"));
+    if (numeroUtente <= 20 && numeroUtente > 0) {
+      if (!listaUtente.includes(numeroUtente)) {
+        numeroUtente = parseInt(prompt("inserisci numero"));
+        listaUtente.push(numeroUtente);
+      }
+    }else {
+      alert("inserisci un numero tra 1 e 20")
+    }
+
+  }
+}
+console.log(listaUtente);
 
 function numeri(min,max) {
 return Math.floor(Math.random() * (max - min) ) + min;
